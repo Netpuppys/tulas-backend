28b83aa87d9ee4786601670a93a07e9d562d8ee1
Harsh Kumar rattewal <harshrattewal@gmail.com>
2024-12-16 11:57:25 +0530

    port added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 // Initializing Express App
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware Setup
 app.use(cors());
